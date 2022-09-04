5e79ff98de32986e644d10792b49dbd987347d04
Esra Akbulat <a.esra.akbulat@gmail.com>
2022-09-04 20:35:32 +0300

    palindrome added

---
+++ Day5-Palindrome Checker/script.js
@@ -0,0 +1,19 @@
+//html elements
+let result = document.querySelector(".result");
+let word = document.querySelector(".word");
+
+//function
+function isPalindrome() {
+  console.log("basarili");
+  let checkWord = word.value.split("").reverse().join("");
+
+  if (word.value === "") {
+    result.innerHTML = "Please enter a word";
+  } else if (word.value === checkWord) {
+    result.innerHTML = `${word.value.toUpperCase()} is a Palindrome`;
+  } else {
+    result.innerHTML = `${word.value.toUpperCase()} is NOT a Palindrome`;
+  }
+}
+//if i start to delete word
+word.addEventListener("keyup", isPalindrome);
