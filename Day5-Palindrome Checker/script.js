7db59ccc0038eb45c05b40fb93faa75f4bade38c
Esra Akbulat <a.esra.akbulat@gmail.com>
2022-09-04 23:22:39 +0300

    palindrome added

--- Day5-Palindrome Checker/script.js
+++ Day5-Palindrome Checker/script.js
@@ -4,7 +4,6 @@ let word = document.querySelector(".word");
 
 //function
 function isPalindrome() {
-  console.log("basarili");
   let checkWord = word.value.split("").reverse().join("");
 
   if (word.value === "") {
